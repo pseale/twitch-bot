6699ca1c5265e24a7dbf220b81aec68e4a0b4900
Peter Seale <peter@pseale.com>
2024-10-12 12:40:57 -0500

    add minimal Twitch chat bot

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# twitch-bot
-Another twitch bot
+# Dead-Simple Twitch Bot skeleton in .NET 8
+
+This is a minimal twitch bot with logging and error handling. It currently connects to one channel and logs chat messages to the console.
+
+# Installation
+
+1. Register a Twitch Application on the [Twitch Developer Console](https://dev.twitch.tv/console/apps).
+2. Install and configure the Twitch CLI.
+    - Install: https://dev.twitch.tv/docs/cli/
+    - Configure: https://dev.twitch.tv/docs/cli/configure-command/
+3. Set the following environment variables:
+    - `TWITCH_BOT_USERNAME`
+    - `TWITCH_CHANNEL`
+4. Store a twitch user authentication token in the environment variable `TWITCH_USER_ACCESS_TOKEN` using one of two options:
+    - Do it manually
+    - Run `./scripts/Get-TwitchToken.ps1`
+5. Run
+    ``` PowerShell
+    cd ./src/TwitchBot.Worker
+    dotnet run
+    ```
+
+# Notes
+
+- This has not been fully tested. Notably, I have never seen a `Disconnected` event or an `Error` event.
+- The Twitch user authentication token does not refresh itself, and needs to be refreshed regularly.
+
---
+++ src/TwitchBot.WorkerService/Worker.cs
@@ -0,0 +1,73 @@
+using TwitchLib.Client;
+using TwitchLib.Client.Events;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace TwitchBot.WorkerService;
+
+public class Worker(ILogger<Worker> logger) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var twitchBotUsername = Environment.GetEnvironmentVariable("TWITCH_BOT_USERNAME");
+        if (string.IsNullOrWhiteSpace(twitchBotUsername))
+            throw new Exception("TWITCH_BOT_USERNAME environment variable is missing");
+
+        var twitchChannel = Environment.GetEnvironmentVariable("TWITCH_CHANNEL");
+        if (string.IsNullOrWhiteSpace(twitchBotUsername))
+            throw new Exception("TWITCH_CHANNEL environment variable is missing");
+
+        var userAccessToken = Environment.GetEnvironmentVariable("TWITCH_USER_ACCESS_TOKEN");
+        if (string.IsNullOrWhiteSpace(userAccessToken))
+            throw new Exception("TWITCH_USER_ACCESS_TOKEN environment variable is missing");
+
+        var credentials = new ConnectionCredentials(twitchBotUsername, userAccessToken);
+        var clientOptions = new ClientOptions
+        {
+            MessagesAllowedInPeriod = 750,
+            ThrottlingPeriod = TimeSpan.FromSeconds(30)
+        };
+        var customClient = new WebSocketClient(clientOptions);
+        var client = new TwitchClient(customClient);
+        client.Initialize(credentials, twitchChannel);
+
+        async void OnClientOnOnJoinedChannel(object? _, OnJoinedChannelArgs e)
+        {
+            logger.LogInformation("TwitchLib: joined channel: " + e.Channel + " " + e.BotUsername);
+        }
+
+        var fatalErrorMessage = "";
+
+        client.OnJoinedChannel += OnClientOnOnJoinedChannel;
+        client.OnMessageReceived += (_, receivedArgs) =>
+            logger.LogDebug("TwitchLib message received: " + receivedArgs.ChatMessage.Message);
+        client.OnConnectionError += (_, errorArgs) =>
+        {
+            fatalErrorMessage = errorArgs.Error.Message;
+            logger.LogError("TwitchLib connection error: " + fatalErrorMessage);
+        };
+        client.OnError += (_, errorArgs) =>
+        {
+            fatalErrorMessage = errorArgs.Exception.ToString();
+            logger.LogError("TwitchLib connection error: " + fatalErrorMessage);
+        };
+        client.OnDisconnected += (_, _) => fatalErrorMessage = "Disconnected";
+        client.OnLog += (_, logArgs) =>
+        {
+            if (logArgs.Data.Contains("PONG"))
+                logger.LogTrace("TwitchLib log message: " + logArgs.Data);
+            else
+                logger.LogDebug("TwitchLib log message: " + logArgs.Data);
+        };
+
+        client.Connect();
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (!string.IsNullOrWhiteSpace(fatalErrorMessage))
+                break;
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
