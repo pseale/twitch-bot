ec308547c4619a89d26d238a3f93c9095f386622
Peter Seale <peter@pseale.com>
2024-10-19 16:02:55 -0500

    VS Code's linter complains about any use of aliased cmdlets and I hate it but the lesser of two evils is to give in to the linter

--- scripts/Get-TwitchToken.ps1
+++ scripts/Get-TwitchToken.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 $text = twitch token -u --scopes 'moderation:read user:read:email chat:edit chat:read' 2>&1
 if (!$?) { throw "Error getting a twitch token: $text" }
 
-$token = ($text | sls 'User Access Token:').ToString().Split(" ")[-1]
+$token = ($text | Select-String 'User Access Token:').ToString().Split(" ")[-1]
 [Environment]::SetEnvironmentVariable("TWITCH_USER_ACCESS_TOKEN", $token)
 
 
