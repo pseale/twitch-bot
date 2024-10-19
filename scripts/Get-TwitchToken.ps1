56e87b2533c8649d5f7c1a2095c8e7e222c07abd
Peter Seale <peter@pseale.com>
2024-10-19 16:01:52 -0500

    reduce requested scopes; fix bug

--- scripts/Get-TwitchToken.ps1
+++ scripts/Get-TwitchToken.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = "Stop"
 
-$text = twitch token -u --scopes 'channel:manage:broadcast channel:read:subscriptions moderation:read user:read:email chat:edit chat:read' 2>&1
-if ($?) { throw "Error getting a twitch token" }
+$text = twitch token -u --scopes 'moderation:read user:read:email chat:edit chat:read' 2>&1
+if (!$?) { throw "Error getting a twitch token: $text" }
 
 $token = ($text | sls 'User Access Token:').ToString().Split(" ")[-1]
 [Environment]::SetEnvironmentVariable("TWITCH_USER_ACCESS_TOKEN", $token)
